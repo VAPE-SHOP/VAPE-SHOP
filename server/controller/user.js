dda08a98a3eabc44b1e427b0959951739f659f3e
Saif-Ayari <saifayari312@gmail.com>
2022-12-04 19:43:18 +0100

    start

---
+++ server/controller/user.js
@@ -0,0 +1,20 @@
+const User = require('../models/user');
+
+export const login = async (req, res) => {
+  try {
+  } catch (err) {}
+};
+
+export const signup = async (req, res) => {
+    try {
+    } catch (err) {}
+  };
+
+  export const updateUser = async (req, res) => {
+    try {
+    } catch (err) {}
+  };
+  export const getUserById = async (req, res) => {
+    try {
+    } catch (err) {}
+  };
