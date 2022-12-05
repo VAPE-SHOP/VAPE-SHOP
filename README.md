e547ed250bcfaa52d59c6d1efbde4bdbc44000f0
Saif-Ayari <saifayari312@gmail.com>
2022-12-05 11:53:40 +0100

    edit mongodb

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # VAPE-SHOP
+
+## Before You Begin
+
+# for your client side
+
+- cd /client
+- Run npm i
+- npm run dev
+
+# for your server side
+
+- cd /server
+- run npm i
+- run start
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import NavBar from './NavBar';
+
+const Home = () => {
+  return <NavBar />;
+};
+
+export default Home;
