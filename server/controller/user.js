4886613ff3f847a20a785e4bef6a254507ce1a24
macbook air <macbookair@MacBook-Air-de-macbook.local>
2022-12-06 13:33:02 +0100

    test

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,14 +1,49 @@
 const User = require('../models/user');
-
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const login = async (req, res) => {
+
+const {email,password} = req.body; 
   try {
-  } catch (err) {}
+    await User.findOne({email}).then (async(user) =>{
+      if(!user) {
+        res.status(404).json({
+          message:"Email does not exist ! please Enter the right Email or You can create account"})
+        }
+        const passwordcom = bcrypt.compare(password,user.password)
+        if(!passwordcom){
+          res.status(404).json({message:"Wrong Password"})
+        }else {
+        const token = jwt.sign({ _id: user._id }, 'shhhhh');
+        res.status(201).json({token ,message:"welcom to your home page"})
+      }
+      })
+      } catch (err) {
+       res.status(404).json({message:"samthing is wrong "})
+  }
 };
 
 const signup = async (req, res) => {
+const {email,password} =req.body
   try {
-  } catch (err) {}
+    await User.findOne({email}).then(async(emailExist) =>{
+      if (emailExist){
+        res.status(404).json({message:'email already is use '});
+
+      }else{
+      const hash = await bcrypt.hash(password, 10)
+        await User.create({
+          email,
+          password:hash
+        })
+        res.status(201).json({message: "user added with success "})
+      }
+    })
+      } catch (err) {
+       res.status(400).json({message:"something is wrong"})
+     }
 };
+
 
 const updateUser = async (req, res) => {
   try {
--- server/models/user.js
+++ server/models/user.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  userName: { type: String, required: true, minlength: 5, maxlength: 50 },
-  password: { type: String, required: true, minlength: 5, maxlength: 1024 },
-  phoneNumber: Number,
-  adress: String,
+  email: { type: String, required: true , unique:true},
+  password: { type: String, required: true },
   role: {
     type: String,
     enum: ['user', 'admin'],
   },
-});
+},
+  {
+  timestamps:true
+  },
+);
 
-const User = mongoose.model('User', userSchema);
-module.exports = User;
+module.exports =  mongoose.model('user', userSchema);
