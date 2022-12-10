4f0a7e888f41000fc2e08c5af2338c52ab741181
macbook air <macbookair@MacBook-Air-de-macbook.local>
2022-12-10 15:36:15 +0100

    fix app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,25 @@
-import { useState,useEffect } from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-import Home from './components/Home/Home.jsx';
-import Login from './components/Login/Login';
+import { useState,useEffect } from 'react'; //
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from './components/Home/Home.jsx'; //
+import Login from './components/Login/Login'; // 
 
-import Vapes from './components/Vapes/Vapes.jsx';
+import Vapes from './components/Vapes/Vapes.jsx'; //
+import Register from '../componet/Register.jsx'; //
+import Admin  from './components/Admin/Admin.jsx'; // 
+import NotFound from './components/NotFound/NotFound.jsx'; //
+import NoAccess from './components/NoAccess/NoAccess.jsx'; //
+import AdminRouter from '../componet/AdminRouter.jsx';//
+import PriveteRouter from '../componet/PriveteRouter.jsx'; // 
+import ForceRedirect from '../componet/ForceRedirect.jsx'; //
 import OneVape from './components/Vapes/OneVape.jsx';
 import PostVape from './components/Vapes/Postvape.jsx';
 import Footer from './components/Footer/Footer.jsx';
 import ManageLiquid from './components/Admin/ManageLiquid.jsx';
-import Admin from './components/Admin/Admin.jsx';
+
 import OneLiquid from './components/Liquid/OneLiquid.jsx';
 import Cart from './components/Cart/Cart.jsx';
 import About from './components/About/About.jsx';
-import NavBar from './components/NavBar/NavBar.jsx';
+import NavBar from './components/NavBar/NavBar.jsx'; //
 import Liquid from './components/Liquid/Liquid.jsx';
 
 
@@ -25,8 +32,13 @@ function App() {
   const [liquid, setLiquid] = useState([]);
   const [oneLiquid, setOneLiquid] = useState([]);
   const [cart, setCart] = useState([]);
-
-
+  const [isConnected ,setIsconnected]=useState(false)
+  const [role ,setRole]=useState()
+    const user={
+    isConnected:isConnected,
+    role:role
+  }
+  
   useEffect(() => {
     getvapes();
     getLiquids();
@@ -70,42 +82,67 @@ function App() {
       setOneLiquid(result.data);
     });
   };
+  const checkUserToken = () => {
+    if (typeof window !== "undefined") {
+      const user = JSON.parse(localStorage.getItem("role"));
+      if (user) {
+        setIsconnected(true);
+        setRole(user)
+      } else {
+        setIsconnected(false);
+      }
+    }
+  };
+  const Logout =()=>{
+   
+    localStorage.clear();
+    setIsconnected(false)
+  
+}
+useEffect(() => {
+  checkUserToken();
+}, [isConnected]);
 
 
 
 
   return (
-    <>
-      <NavBar size={cart.length} />
+    <BrowserRouter>
+    <div class="bg-light" style={{height: "100vh"}}>
+      <NavBar size={cart.length} user={user} Logout={Logout}/>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/admin" element={<Admin />} />
+        <Route path="/" element={<PriveteRouter user={user}><Home/></PriveteRouter>} />
+        <Route path="/admin" element={ <AdminRouter user={user}><Admin /> </AdminRouter>} />
         <Route path="/manageLiquid" element={<ManageLiquid liquid={liquid} />} />
         <Route path="/login" element={<Login />} />
         <Route
           path="/liquid"
-          element={
+          element={<PriveteRouter user={user}>
             <Liquid
               liquids={liquid}
               one={getOneLiquid}
               handleClick={handleClick}
             />
-          }
+          </PriveteRouter>}
         />
+        <Route  path="/*" element={<NotFound />}/>
+        <Route  path="/noaccess" element={<NoAccess />}/>
         <Route
           path="/oneLiquid"
           element={<OneLiquid liquidOne={oneLiquid} />}
         />
         <Route path="/About" element={<About />} />
 
-
+       
       <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/vapes" element={<Vapes 
+      <Route path="/login" element={<ForceRedirect user={user}><Login /></ForceRedirect>} />
+      <Route  path="/register" element={<ForceRedirect user={user}><Register /></ForceRedirect>}/>
+       
+      <Route path="/vapes" element={<PriveteRouter user={user}><Vapes 
        Vape={Vape}
        one={getOneVape}
        handleClick={handleClick}
-      />} />
+      /> </PriveteRouter> } />
       <Route path="/OneVape" element={<OneVape oneVape={oneVape} />} />
       
       <Route path="/PostVape" element={<PostVape />} />
@@ -119,7 +156,8 @@ function App() {
         />
       </Routes>
       <Footer />
-    </>
+    </div>
+    </BrowserRouter>
   );
 }
 export default App
---
+++ client/src/components/Register/Register.jsx
@@ -0,0 +1,92 @@
+import React,{useState} from "react";
+import { Link,useNavigate } from "react-router-dom";
+import Inputs from "../../componet/Inputs"
+import axios from "axios";
+function Register() {
+    const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(false);
+    const [form, setForm] = useState({});
+    const [errors, setErrors] = useState({});
+    const onChangeHandler = (event) => {
+        setForm({
+          ...form,
+          [event.target.name]: event.target.value,
+        });
+      };
+    
+      const onSubmitHandler = (event) => {
+        setIsLoading(true);
+        event.preventDefault();
+        axios
+          .post("http://localhost:8080/user/signup", form)
+          .then((response) => {
+            alert(response.data.message)
+            setTimeout(() => {
+              setIsLoading(false);
+              navigate("/login");
+            }, 1000);
+          })
+          .catch((err) => {
+            setErrors(err.response.data);
+            setIsLoading(false);
+          });
+      };
+  return (
+    <div class="container p-4 mt-4">
+      <div class="row justify-content-evenly mt-4">
+        <div class="col-lg-6 col-md-12 mt-4">
+          <div class="d-flex">
+            <i class="fa-solid fa-right-to-bracket fs-1 mx-2"></i>{" "}
+            <h2>Register</h2>
+          </div>
+          <div
+            class="p-6 shadow-lg p-3 mb-5 bg-body rounded"
+            style={{ backgroundColor: "white" }}
+          >
+            <form onSubmit={onSubmitHandler}>
+              <Inputs
+                name="name"
+                label="Name"
+                type="text"
+                icon="fa-solid fa-user"
+                onChange={onChangeHandler}
+              />
+              <Inputs
+                name="email"
+                label="Email"
+                type="text"
+                icon="fa-solid fa-key"
+                onChange={onChangeHandler}
+              
+              />
+              <Inputs
+                name="password"
+                label="Password"
+                type="password"
+                icon="fa-solid fa-user"
+                onChange={onChangeHandler}
+
+              />
+              <Inputs
+                name="confirm"
+                label="Confirm password"
+                type="text"
+                icon="fa-solid fa-user"
+                onChange={onChangeHandler}
+
+              />
+              <div class="d-flex justify-content-between">
+                <button type="submit" class="btn btn-outline-primary">
+                  Save <i class="fa-solid fa-floppy-disk"></i>
+                </button>
+                <Link to="/login">I have account</Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
