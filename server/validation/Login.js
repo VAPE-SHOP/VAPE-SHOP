058ecdfcca36db8ce7bb0e978a547f68d071f38a
macbook air <macbookair@MacBook-Air-de-macbook.local>
2022-12-10 14:08:32 +0100

    add folder validator

---
+++ server/validation/Login.js
@@ -0,0 +1,30 @@
+const isEmpty = require ('./isEmpty');
+const validator = require ('validator');
+
+
+
+module.exports = function ValidateLogin(data){  /// data is req.body 
+ let error = {}; 
+
+ data.email = !isEmpty (data.email) ? data.email : ""
+ data.password = !isEmpty (data.password) ? data.password : ""
+
+
+ 
+  if (!validator.isEmail(data.email)){
+    error.email ="Required format email "
+   }
+   if (validator.isEmpty(data.email)){
+    error.email ="Required email"
+   }
+   if (validator.isEmpty(data.password)){
+    error.password ="Required password"
+   }
+ 
+  
+
+   return {
+    error,
+    isValid: isEmpty(error)
+   }
+}
