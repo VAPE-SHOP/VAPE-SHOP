6234eba7002b7f24a560e7e515769c33811ebc21
macbook air <macbookair@MacBook-Air-de-macbook.local>
2022-12-10 13:56:19 +0100

    fix controllers login and register

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,56 +1,80 @@
-const User = require('../models/user');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const login = async (req, res) => {
+const UserModel = require("../models/user")
+const ValidateRegister = require('../validation/Register')
+const ValidateLogin = require('../validation/Login')
+const jwt = require ("jsonwebtoken")
+const bcrypt = require ('bcryptjs')
+const { exists } = require("../models/user")
 
-const {email,password} = req.body; 
-  try {
-    await User.findOne({email}).then (async(user) =>{
-      if(!user) {
-        res.status(404).json({
-          message:"Email does not exist ! please Enter the right Email or You can create account"})
-        }
-        const passwordcom = bcrypt.compare(password,user.password)
-        if(!passwordcom){
-          res.status(404).json({message:"Wrong Password"})
-        }else {
-        const token = jwt.sign({ _id: user._id }, 'shhhhh');
-        res.status(201).json({token ,message:"welcom to your home page"})
-      }
-      })
-      } catch (err) {
-       res.status(404).json({message:"samthing is wrong "})
-  }
-};
 
-const signup = async (req, res) => {
-const {email,password} =req.body
-  try {
-    await User.findOne({email}).then(async(emailExist) =>{
-      if (emailExist){
-        res.status(404).json({message:'email already is use '});
-
-      }else{
-      const hash = await bcrypt.hash(password, 10)
-        await User.create({
-          email,
-          password:hash
+const Login = async(req,res)=>{
+  const {error,isValid} = ValidateLogin(req.body)
+try {
+  if(!isValid){
+      res.status(404).json(error)
+  }else{
+UserModel.findOne({email:req.body.email})
+.then(user=>{
+  if(!user){
+      error.email = "not found user"
+  res.status(404).json(error)
+  }else {
+      bcrypt.compare(req.body.password ,user.password)
+      .then(isMatch =>{
+          if(!isMatch){
+         error.password = "incorrect password"
+         res.status(404).json(error)
+          }else{ 
+      var token = jwt.sign({
+        id :user._id,
+        name:user.name,
+        email:user.email,
+        role:user.role
+          },process.env.PRIVETE_KEY ,{expiresIn : '1h'});    
+        res.status(200).json({
+          message:'success',
+          role :user.role,
+          token,
+          
         })
-        res.status(201).json({message: "user added with success "})
-      }
-    })
-      } catch (err) {
-       res.status(400).json({message:"something is wrong"})
-     }
-};
+        // res.cookie("Authorization", token, {
+        //   expires: new Date(exp),
+        //   httpOnly: true,
+        //   sameSite: "lax",
+        //   secure: process.env.NODE_ENV === "production",
+        // });
+          }
+      })
+  }
+})
+}
+}catch(error){
+  res.status(404).json(error.message)
+}
+}
 
-
-const updateUser = async (req, res) => {
+const Register  = async(req,res)=>{
+  const {error,isValid} = ValidateRegister(req.body)
   try {
-  } catch (err) {}
-};
-const getUserById = async (req, res) => {
-  try {
-  } catch (err) {}
-};
-module.exports = { login, signup, updateUser, getUserById };
+    if (!isValid){
+      res.status(404).json(error)
+    }
+    else {
+      UserModel.findOne({email:req.body.email})
+      .then(async(exist) =>{
+          if(exist){
+              error.email = "user exist";
+              res.status(404).json(error)
+          }else {
+              const hash = bcrypt.hashSync(req.body.password , 10) // hashed password
+              req.body.password = hash
+              req.body.role = "USER"
+              await UserModel.create(req.body)
+              res.status(200).json({message : "success"})
+          }
+      })
+    }
+  }catch(error){
+      res.status(404).json(error.message)
+  }
+  }
+module.exports = { Login, Register };
